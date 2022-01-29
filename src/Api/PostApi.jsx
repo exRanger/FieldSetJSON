8fc16eee591326aed648294eb6004df0c6dc4ecf
Zagirov Il'giz <lmnasxzz@gmail.com>
2022-01-29 16:45:19 +0300

    Update PostApi.jsx

--- src/Api/PostApi.jsx
+++ src/Api/PostApi.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 export default class PostApi {
      static async getAllPosts(limit = 10, page = 1 ){
             const response = await axios.get('https://jsonplaceholder.typicode.com/posts',
+            /* set params */
             {
                params: {
                    _limit: limit,
