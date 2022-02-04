e22813beee6b4a7c55138417ae9dd32ea91e5d18
Zagirov Il'giz <lmnasxzz@gmail.com>
2022-02-04 09:15:24 +0300

    fix whitespace

--- src/components/UI/modale/MyWindow.jsx
+++ src/components/UI/modale/MyWindow.jsx
@@ -8,9 +8,9 @@ export default function MyWindow({children, visible, setVisible}) {
     if(visible){
         rootClasses.push(cl.active)
     }
+    
     return (
         <div className={rootClasses.join(' ')} onClick={()=> { setVisible(false)}}>
-
             <div className={cl.MyWindowContent} onClick={(e)=> { e.stopPropagation()}}>{children}</div>
         </div>
     )
