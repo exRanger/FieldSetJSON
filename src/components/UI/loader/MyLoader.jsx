a99a07e9dcbedd65683958694324a3b5b0282c0e
Zagirov Il'giz <lmnasxzz@gmail.com>
2022-02-04 09:14:54 +0300

    Update MyLoader.jsx

--- src/components/UI/loader/MyLoader.jsx
+++ src/components/UI/loader/MyLoader.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import cl from './MyLoader.module.css'
+
 export default function MyLoader() {
     return (
         <div className={cl.MyLoader}>{/* css animation via keyframes */}</div> 
