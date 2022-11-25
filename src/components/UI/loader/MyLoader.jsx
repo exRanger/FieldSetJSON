47f72ac3ae3134403ae3a722df82b2e9b767e06f
Zagirov Il'giz <lmnasxzz@gmail.com>
2022-11-25 22:56:46 +0300

    Update MyLoader.jsx

--- src/components/UI/loader/MyLoader.jsx
+++ src/components/UI/loader/MyLoader.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import cl from './MyLoader.module.css'
+import React from 'react';
+import cl from './MyLoader.module.css';
 
 export default function MyLoader() {
     return (
