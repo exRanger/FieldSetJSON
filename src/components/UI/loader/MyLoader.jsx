f99fdcf9face788937304616c925e7981b8e570e
Zagirov Il'giz <lmnasxzz@gmail.com>
2023-07-22 22:53:35 +0300

    Update MyLoader.jsx

--- src/components/UI/loader/MyLoader.jsx
+++ src/components/UI/loader/MyLoader.jsx
@@ -3,6 +3,6 @@ import cl from './MyLoader.module.css';
 
 export default function MyLoader() {
     return (
-        <div className={cl.MyLoader}>{/* css animation via keyframes */}</div> 
+        <div className={cl.MyLoader}>{/* css animation via keyframes */}</div>
     )
 }
